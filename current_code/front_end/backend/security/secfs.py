d934c71cd227169f50eb01672dc9f42415f29bf3
jonbloomfield <35470400+jonbloomfield@users.noreply.github.com>
2021-01-29 17:50:08 +0000

    security fuctions update
    profile data encrpytion/decryption fully fuction and based on user password.  scable profile vars ready for chat system intergration.

--- current_code/front_end/backend/security/secfs.py
+++ current_code/front_end/backend/security/secfs.py
@@ -1,11 +1,24 @@
-from .. import threadqueues
-from passlib.hash import sha256_crypt
 import os
-from stat import S_IWRITE, S_IREAD
+from stat import S_IREAD, S_IWRITE
+
+from Crypto.Protocol.KDF import PBKDF2
+from passlib.hash import sha256_crypt
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+from Crypto.Random import get_random_bytes
+
+from .. import threadqueues
+
 masterpassword="$5$rounds=535000$dJMrUQM9r2bsxEmX$qlH/zMQJ.P1LcnlVeHz2dP8pjsdCh2aP5FMeU7bJeX0"
 helpstring="SEC CONSOLE MODE.  COMMANDS AS FOLLOWS:\n CREATE NEW PROFILE: 'newprofile'\n "
 helpstring+="CHANGE PASSWORD: 'changepassword' \n HELP (THESE OPTIONS) 'help'.\n"
 helpstring+="PLEASE NOTE - WHILE SECURITY FUNCTIONS ARE UNDERWAY, NORMAL REQUESTS ARE ON HOLD UNTIL SECURITY FUNCTION COMPLETED.-SEC-"
+currentprofilename=""
+currentprofileprivs=""
+currentprofileloc=""
+
+
 
 def passwordcheck(password,profilename):
 	storedpassword=""
@@ -26,13 +39,28 @@ def wait_for_secure_input():
 	return threadqueues.secureinput.get()
 
 def newprofile():
-	adminflag=False 	
-	threadqueues .secureoutput.put("please enter profile name")
-	pname=wait_for_secure_input()
-	threadqueues.secureoutput.put("PROFILE NAME: "+pname+". Please enter profile password")
-	pps=wait_for_secure_input()
-	threadqueues.secureoutput.put("please confirm password")
-	ppscheck=wait_for_secure_input()
+	adminflag=False
+	pnameok=False
+	while pnameok==False:	
+		threadqueues.secureoutput.put("please enter profile name")
+		pname=wait_for_secure_input()
+		fstring="/media/sf_share/samaritan-VA/current_code/front_end/backend/security/profiles/"+pname+".txt"
+		try:
+			f = open(fstring,"r")
+			f.close()
+			threadqueues.secureoutput.put("PROFILE NAME ALREADY IN USE. PLEASE TRY AGAIN.")
+		except FileNotFoundError:
+			pnameok=True
+	pscheckexit=0
+	while(pscheckexit==0):
+		threadqueues.secureoutput.put("PROFILE NAME: "+pname+". Please enter profile password")
+		pps=wait_for_secure_input()
+		threadqueues.secureoutput.put("please confirm password")
+		ppscheck=wait_for_secure_input()
+		if(ppscheck==pps):
+			pscheckexit=1
+		else:
+			threadqueues.secureoutput.put("ERROR. PASSWORDS DON'T MATCH. PLEASE TRY AGAIN.")
 	threadqueues.secureoutput.put("Do you wish this to be an admistrative account? Y/N")
 	if(wait_for_secure_input())=="Y":
 		exit=1
@@ -54,18 +82,66 @@ def newprofile():
 						exit=0
 					else:threadqueues.secureoutput.put("invalid input. please input Y/N.")
 
-	if(ppscheck==pps):
-		storedpassword=sha256_crypt.hash(pps)
-		passwordfile = open("/media/sf_share/samaritan-VA/current_code/front_end/backend/security/keymanagement/pswds.txt", "a")
-		passwordfile.write("\n"+pname+":"+storedpassword)
-		passwordfile.close()
-		profilelistfile=open("/media/sf_share/samaritan-VA/current_code/front_end/backend/security/profiles/profilelist.txt","a")
-		if adminflag==True:privs="1011010"
-		if adminflag==False:privs="0000000"
-		profilelistfile.write("\n"+pname+":"+privs)
-		print("profile created")
-
+	threadqueues.secureoutput.put("please enter default location (this can be changed later)")
+	loc=wait_for_secure_input()
+	storedpassword=sha256_crypt.hash(pps)
+	passwordfile = open("/media/sf_share/samaritan-VA/current_code/front_end/backend/security/keymanagement/pswds.txt", "a")
+	passwordfile.write("\n"+pname+":"+storedpassword)
+	passwordfile.close()
+	profilelistfile=open("/media/sf_share/samaritan-VA/current_code/front_end/backend/security/profiles/profilelist.txt","a")
+	if adminflag==True:privs="1011010"
+	if adminflag==False:privs="0000000"
+	profilelistfile.write("\n"+pname+":"+privs)
+	fstring="/media/sf_share/samaritan-VA/current_code/front_end/backend/security/profiles/"+pname+".txt"
+	f=open(fstring,"wb")
+	profilestringlist=[pname,privs,loc]
+	profilestring=""
+	for i in profilestringlist:
+		profilestring+=(":"+i)
+	salt=get_random_bytes(32)
+	filekey= PBKDF2(pps, salt, dkLen=32)
+	data = bytes(profilestring,"utf-8")
+	cipher = AES.new(filekey, AES.MODE_CBC) # Create a AES cipher object with the key using the mode CBC
+	ciphered_data = cipher.encrypt(pad(data, AES.block_size)) # Pad the input data and then encrypt
+	f.write(salt)
+	f.write(cipher.iv)
+	f.write(ciphered_data)
+	f.close()
+	print("profile created")
 	return "nptest"
+#32 for salt
+#16 for iv
+
+def retriveprofiledata():
+	threadqueues.secureoutput("please enter profile name")
+	pname=wait_for_secure_input()
+	threadqueues.secureoutput("please enter password")
+	password=wait_for_secure_input()
+	pnamefile="/media/sf_share/samaritan-VA/current_code/front_end/backend/security/profiles/"+pname+".txt"
+	try:
+		profilefile=open(pnamefile,"rb")
+	except FileNotFoundError:
+		threadqueues.secureoutput.put("error.  name or password incorrect. please try again.")
+		return
+	passcheck=passwordcheck(password,pname)
+	if passcheck==False:
+		threadqueues.secureoutput.put("error.  name or password incorrect. please try again.")
+		return
+	iv = profilefile.read(16)
+	salt=profilefile.read(32)
+	ciphertext=profilefile.read()
+	profilefile.close()
+	key = PBKDF2(password, salt, dkLen=32) 
+	cipher = AES.new(key, AES.MODE_CBC, iv=iv)  # Setup cipher
+	original_data = unpad(cipher.decrypt(ciphertext), AES.block_size) # Decrypt and then up-pad the result
+	profiledata=original_data.split(":")
+	global currentprofilename
+	global currentprofileprivs
+	global currentprofileloc
+	currentprofilename=profiledata[0]
+	currentprofileprivs=profiledata[1]
+	currentprofileloc=profiledata[2]			
+
 def changepassword():
 	return "changepassword test"
 def helpoptions():
@@ -73,6 +149,9 @@ def helpoptions():
 
 
 def secmain(inp):
+	if inp=="":
+		output="INPUT NOT RECOGNISED.\n"+helpstring
+		return output
 	output=secmaindict[inp]()
 	if output==None:
 		output="INPUT NOT RECOGNISED.\n"+helpstring
--- current_code/front_end/test.py
+++ current_code/front_end/test.py
@@ -1,5 +1,54 @@
+from Crypto.Protocol.KDF import PBKDF2
 from passlib.hash import sha256_crypt
-def newprofile(): 	
-    toredpassword=sha256_crypt.hash("haroldfinch")
-    print(toredpassword)
-newprofile()
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+from Crypto.Random import get_random_bytes
+
+masterpassword="$5$rounds=535000$dJMrUQM9r2bsxEmX$qlH/zMQJ.P1LcnlVeHz2dP8pjsdCh2aP5FMeU7bJeX0"
+
+
+def passwordcheck(password,profilename):
+	storedpassword=""
+	if(profilename=="masteradmin"):
+		storedpassword=masterpassword
+	passwordfile = open("/media/sf_share/samaritan-VA/current_code/front_end/backend/security/keymanagement/pswds.txt", "r")
+	for line in passwordfile:
+		if profilename in line:
+			splitline=line.split(":")
+			storedpassword=splitline[1]
+			break
+	if storedpassword=="": return 0
+	return sha256_crypt.verify(password,storedpassword)
+
+
+print("please enter profile name")
+pname=input()
+print("please enter password")
+password=input()
+pnamefile="/media/sf_share/samaritan-VA/current_code/front_end/backend/security/profiles/"+pname+".txt"
+try:
+	profilefile=open(pnamefile,"rb")
+except FileNotFoundError:
+	print("error.  name or password incorrect. please try again.")
+passwordfilestring="/media/sf_share/samaritan-VA/current_code/front_end/backend/security/keymanagement/pswds.txt"
+passwordfile=open(passwordfilestring,"r")
+lines = passwordfile.readlines()
+storedpassword=""
+for line in lines:
+    line=line.split(":")
+    if(line[0]==pname):storedpassword=line[1]
+print(storedpassword)
+print(passwordcheck(password,pname))
+passcheck=passwordcheck(password,pname)
+if passcheck==False:
+	print("error.  name or password incorrect. please try again.")
+salt=profilefile.read(32)
+iv = profilefile.read(16)
+ciphertext=profilefile.read()
+profilefile.close()
+
+key = PBKDF2(password, salt, dkLen=32) 
+cipher = AES.new(key, AES.MODE_CBC, iv=iv)  # Setup cipher
+original_data = unpad(cipher.decrypt(ciphertext), AES.block_size) # Decrypt and then up-pad the result
+print(original_data)
